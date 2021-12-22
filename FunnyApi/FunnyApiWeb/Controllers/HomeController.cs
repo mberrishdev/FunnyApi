88be9d82106eb1cb68446ae95b1292c68c2d8ee1
m.berishvili <m.berishvili@smartsoft.ge>
2021-12-22 13:26:21 +0400

    add some html

--- FunnyApi/FunnyApiWeb/Controllers/HomeController.cs
+++ FunnyApi/FunnyApiWeb/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace FunnyApiWeb.Controllers
             return View();
         }
 
+        public IActionResult Bored()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
